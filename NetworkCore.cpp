c0777d19efc8bb3528cdf372b574650901daf347
Phillip Maluta <phillip.maluta@volia-software.com>
2017-06-05 10:07:06 +0300

    [NetworkCore] native brindge

--- NetworkCore.cpp
+++ NetworkCore.cpp
@@ -2,6 +2,7 @@
 
 NetworkCore::NetworkCore(QObject *parent) : QObject(parent)
 {
+  _networkManager = QSharedPointer<QNetworkAccessManager>(new QNetworkAccessManager());
 }
 
 void NetworkCore::registrate(const QString& email, const QString& name, const QString& phone){
--- NetworkCore.hpp
+++ NetworkCore.hpp
@@ -2,27 +2,28 @@
 #define NETWORKCORE_HPP
 
 #include <QObject>
+#include <QtNetwork>
 
 class NetworkCore : public QObject
 {
   Q_OBJECT
 
-  //Q_PROPERTY(QImage avatar READ avatar WRITE setAvatar NOTIFY avatarChanged)
-
 public:
   explicit NetworkCore(QObject *parent = 0);
-  //QImage avatar() const;
-
   Q_INVOKABLE void registrate(const QString& email, const QString& name, const QString& phone);
   Q_INVOKABLE void authorize();
 
 signals:
-  //void avatarChanged();
   void authorized();
   void registered();
 
 public slots:
-  //void setAvatar(const QImage& img);
+
+protected:
+  void connectToHost();
+
+private:
+  QSharedPointer<QNetworkAccessManager> _networkManager;
 };
 
 #endif // NETWORKCORE_HPP
