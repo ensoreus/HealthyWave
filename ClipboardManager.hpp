16534e466b3e70bd38ee397e233e9cdc6d4b6f13
Phillip Maluta <phillip.maluta@volia-software.com>
2017-10-10 08:29:06 +0300

    [Clipboard] empty signal

--- ClipboardManager.hpp
+++ ClipboardManager.hpp
@@ -9,29 +9,35 @@ class ClipboardManager : public QObject
   Q_OBJECT
   Q_PROPERTY(bool empty READ isEmpty NOTIFY isEmptyChanged)
 
- public:
-     explicit ClipboardManager(QObject *parent = 0) : QObject(parent) {
-         clipboard = QGuiApplication::clipboard();
-     }
+public:
+  explicit ClipboardManager(QObject *parent = 0) : QObject(parent) {
+    clipboard = QGuiApplication::clipboard();
+    connect(clipboard, SIGNAL(dataChanged()), this, SLOT(textChanged()));
+  }
 
-     Q_INVOKABLE void setText(QString text){
-         clipboard->setText(text, QClipboard::Clipboard);
-         clipboard->setText(text, QClipboard::Selection);
-     }
+  Q_INVOKABLE void setText(QString text){
+    clipboard->setText(text, QClipboard::Clipboard);
+    clipboard->setText(text, QClipboard::Selection);
+  }
 
-    Q_INVOKABLE QString text(){
-        return clipboard->text();
-    }
+  Q_INVOKABLE QString text(){
+    return clipboard->text();
+  }
 
   bool isEmpty() const{
     return clipboard->text().length() == 0;
   }
 
+public slots:
+  void textChanged(){
+    emit isEmptyChanged();
+  }
+
 signals:
   void isEmptyChanged();
 
- private:
-     QClipboard *clipboard;
+private:
+  QClipboard *clipboard;
 };
 
 #endif // CLIPBOARDMANAGER_HPP
